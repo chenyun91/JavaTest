64b89f787c29a273a48781927e5c8f01005299ed
chenyun <chenyun@wzdai.com>
2018-08-23 14:14:57 +0800

    创建分支v3.0

---
+++ src/BranchC/BranchC.java
@@ -0,0 +1,15 @@
+package BranchC;
+
+/**
+ * Copyright (C), 2011-2018 温州贷
+ * FileName: BranchC.BranchC.java
+ * Author: chenyun
+ * Email: chenyun@wzdai.com
+ * Date: 2018/8/23 14:14
+ * Description:
+ * History:
+ * <Author>      <Time>    <version>    <desc>
+ * chenyun      14:14    1.0        Create
+ */
+class BranchC {
+}
